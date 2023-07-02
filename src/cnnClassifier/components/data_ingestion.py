95e09e38613ef6c1276b3011f76ecd8adc63bf92
pranay518 <pranayprabhat518@gmail.com>
2023-07-02 13:59:08 +0530

    training added

--- src/cnnClassifier/components/data_ingestion.py
+++ src/cnnClassifier/components/data_ingestion.py
@@ -18,7 +18,7 @@ class DataIngestion:
             logger.info(f"{filename} download: with following info: \n {headers}")
 
         else:
-            logger.info(f"File already exists of size: {get_size(path(self.config.local_data_file))}")
+            logger.info(f"File already exists of size: {get_size(Path(self.config.local_data_file))}")
 
 
     def extract_zip_file(self):
